f160c5770d6737ef81401b29362c02b80f7f7397
Andrew Brudnak <brudnak@icloud.com>
2019-04-28 05:18:29 -0700

    Go programming practice
    - Short declaration practice
    - Declared variables must be used

---
+++ 001-short-declaration-operator/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Short declaration operator ":="
+	x := 14 // declares variable x and assign value
+	fmt.Println(x)
+	x = 1989 // new value assigned "=" sign only
+	fmt.Println(x)
+	
+	y := 90 + 10 // variables must be used
+	// + is the operator
+	// 90 and 10 are the operands
+	fmt.Println(y)
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Go Practice
+
+Working on learning the Go programming language. Currently a Lambda School student. My goal is to make a web application with a Go backend and React front end.
