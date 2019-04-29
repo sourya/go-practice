7c6f9fe3244a5f610e739bb8d00b41449a32e07b
Andrew Brudnak <brudnak@icloud.com>
2019-04-28 23:53:26 -0700

    Creating custom type

---
+++ 007-exercise-level-one/04-exercise4/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type year int
+var x year // underlying type is int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T is the TYPE of variable x\n", x)
+	x = 2020
+	fmt.Println(x)
+}
+
