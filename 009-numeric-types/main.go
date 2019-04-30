0272f56aba3ddcf6fc7297d5830e51468edd732e
Andrew Brudnak <brudnak@icloud.com>
2019-04-30 00:12:29 -0700

    Go practice
    - Numeric values
    - Runtime pkg

---
+++ 009-numeric-types/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var age byte = 29
+
+// byte = alias for uint8
+// rune = alias for int32
+
+// unsigned integers start at zero i.e. not negative
+
+func main() {
+	fmt.Println(age)
+	fmt.Printf("%T\n", age)
+	fmt.Println(runtime.GOOS) // windows
+	fmt.Println(runtime.GOARCH) // amd64
+	// Dell Laptop
+}
