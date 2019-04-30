d0ab696e048fd84c8acc07d39ce818c31c33354f
Andrew Brudnak <brudnak@icloud.com>
2019-04-30 02:18:46 -0700

    Conversion & numeral systems
    - Practice with byte which alias for uint8
    - Binary and hexadecimal exercise

---
+++ 011-numerial-systems/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "H"
+	fmt.Println(s)
+
+	bs := []byte(s)
+	fmt.Println(bs)
+
+	n := bs[0]
+	fmt.Println(n)
+	fmt.Printf("%T\n", n) // type = uint8
+	fmt.Printf("%b\n", n) // binary
+	fmt.Printf("%#X\n", n) // hex
+}
