c455dcdb227e8afb463c87c4e5947e25d84f859f
Andrew Brudnak <brudnak@icloud.com>
2019-05-06 00:04:35 -0700

    Changed from Println to Printf
    - Setup better formatting to see
    what is happening in the loops
    - Terminal output below:
    
    $ go run main.go
    The outer loop: 0
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 1
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 2
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 3
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 4
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 5
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 6
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 7
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 8
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 9
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2
    The outer loop: 10
                    The inner loop: 0
                    The inner loop: 1
                    The inner loop: 2

--- 017-nested-loops/main.go
+++ 017-nested-loops/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	for i := 0; i <= 10; i++ {
-		fmt.Println("The outer loop: %d\t The inner loop: %d\n", i)
+		fmt.Printf("The outer loop: %d\n", i)
 		for j := 0; j < 3; j++ {
-			fmt.Println("The outer loop: %d\t The inner loop: %d\n", i, j)
+			fmt.Printf("\t\tThe inner loop: %d\n", j)
 		}
 	}
 }
