aed768e4f3ad97246dad742b9ec14d8de63dabb0
Andrew Brudnak <brudnak@icloud.com>
2019-05-09 09:05:23 -0700

    If statement inside of for

--- 018-loop-for-statement/main.go
+++ 018-loop-for-statement/main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+//func main() {
+//	x := 1
+//	for x < 10 {
+//		fmt.Println(x)
+//		x++
+//	}
+//	fmt.Println("done.")
+//}
+
 func main() {
 	x := 1
-	for x < 10 {
+	for {
+		if x > 9 {
+			break
+		}
 		fmt.Println(x)
 		x++
 	}
