809d863e89b48613ff2409a8fc69875b3ec0c057
Andrew Brudnak <brudnak@icloud.com>
2019-05-23 14:01:24 -0700

    Exploring arrays, use slice instead

---
+++ 028-array/01-array-basics/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Have to declare the size of the array
+	var x [5]int
+	var y [6]int
+	fmt.Println(x)
+	x[3] = 19
+	fmt.Println(x)
+	fmt.Println(y)
+
+}
