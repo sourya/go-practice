cc766811d3a35fd513e62313d932b6930268f109
Andrew Brudnak <brudnak@icloud.com>
2019-05-23 14:19:14 -0700

    First slice

---
+++ 029-slice-composite-literal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// COMPOSITE LITERAL
+	x := []int{4, 8, 15, 16, 23, 42}
+	fmt.Println(x)
+}
+
+// a SLICE allows you to group together VALUES of the same TYPE
