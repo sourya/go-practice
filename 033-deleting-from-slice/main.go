65a0ef9232110abe9bf945ddc3baed833a87001a
Andrew Brudnak <brudnak@icloud.com>
2019-05-23 21:30:26 -0700

    Deleting from a splice, use append
    - Remember, ease of programming

---
+++ 033-deleting-from-slice/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{4, 8, 15, 16, 23, 42}
+	fmt.Println(x)
+	x = append(x, 77, 88, 99, 1014)
+	fmt.Println(x)
+
+	y := []int{234, 456, 678, 987}
+	x = append(x, y...)
+	fmt.Println(x)
+
+	// Removes 15 AND 16
+	x = append(x[:2], x[4:]...)
+	fmt.Println(x)
+}
