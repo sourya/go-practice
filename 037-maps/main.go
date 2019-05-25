300d0ed3f5554d120bcbe9e0e0290cb7c2f3511f
Andrew Brudnak <brudnak@icloud.com>
2019-05-25 06:42:42 -0700

    Range over map and slice

--- 037-maps/main.go
+++ 037-maps/main.go
@@ -19,7 +19,19 @@ func main() {
 	fmt.Println(v)
 	fmt.Println(ok)
 
+	m["Todd"] = 33
+
 	if v, ok := m["Jessie"]; ok {
 		fmt.Println("THIS IS THE IF PRINT", v)
+	}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	// range over slice
+	xi := []int{1, 2, 3, 4, 5, 6}
+	for i, v := range xi {
+		fmt.Println(i, v)
 	}
 }
