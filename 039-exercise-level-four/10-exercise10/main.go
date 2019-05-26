335078c24b2c7dd074ae2ffd094336113f51e598
Andrew Brudnak <brudnak@icloud.com>
2019-05-26 00:46:18 -0700

    Delete from map

---
+++ 039-exercise-level-four/10-exercise10/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		"brudnak_andrew": []string{
+			"Computer Science",
+			"Playing with Russell & Ruby",
+			"Spending time with Jessie",
+		},
+		"brudnak_russell": []string{
+			"Watching YouTube",
+			"Painting",
+			"Eating fruit",
+		},
+	}
+
+	x["brudnak_ruby"] = []string{
+		"Being held by mommy",
+		"Seeing new things",
+		"Talking with my parents",
+	}
+
+	delete(x, "brudnak_andrew")
+
+	for i, v := range x {
+		fmt.Printf("%v\n", i)
+		for index, favorite := range v {
+			index++
+			fmt.Printf("\t List number: %v \t Favorite thing: %v \n ", index, favorite)
+		}
+	}
+}
