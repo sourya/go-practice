98190e6130036f9adbefd8c02114e17f02c40cd3
Andrew Brudnak <brudnak@icloud.com>
2019-06-02 12:49:39 -0700

    JSON Marshal

---
+++ 060-json-marshal/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := person{
+		First: "Andrew",
+		Last:  "Brudnak",
+		Age:   29,
+	}
+
+	p2 := person{
+		First: "Russell",
+		Last:  "Brudnak",
+		Age:   2,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
