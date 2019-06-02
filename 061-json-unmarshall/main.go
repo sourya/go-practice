8cd33250de8110034072acf739d31806457084d4
Andrew Brudnak <brudnak@icloud.com>
2019-06-02 13:49:58 -0700

    JSON Unmarshal

---
+++ 061-json-unmarshall/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+
+type person struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
+func main() {
+	s := `[{"First":"Andrew","Last":"Brudnak","Age":29},{"First":"Russell","Last":"Brudnak","Age":2}]`
+
+	bs := []byte(s)
+
+	fmt.Printf("%T\n", s)
+	fmt.Printf("%T\n", bs)
+
+	var people []person
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("All of the data", people)
+
+	for i, v := range people {
+		i++
+		fmt.Println("\nPERSON NUMBER:", i)
+		fmt.Printf("First name: %v\t Last name: %v\t Age: %v\t", v.First, v.Last, v.Age)
+		fmt.Println("")
+	}
+}
