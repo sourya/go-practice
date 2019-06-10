8e0549a60bc935967cc5b66d40ce104d7725c8e1
Andrew Brudnak <brudnak@icloud.com>
2019-06-10 03:27:37 -0700

    Intro to Atomic package

--- 070-atomic/main.go
+++ 070-atomic/main.go
@@ -4,25 +4,26 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	fmt.Println("CPUs:", runtime.NumCPU())
 	fmt.Println("Goroutines:", runtime.NumGoroutine())
 
-	counter := 0
+	var counter int64
 
 	const gs = 100
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
+
+
 	for i := 0; i < gs; i++ {
 		go func() {
-			v := counter
-			// time.Sleep(time.Second)
+			atomic.AddInt64(&counter, 1)
 			runtime.Gosched()
-			v++
-			counter = v
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter))
 			wg.Done()
 		}()
 		fmt.Println("Goroutines:", runtime.NumGoroutine())
