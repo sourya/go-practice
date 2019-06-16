e02a3e67bb94a4d58c5ab3819967d46ca865a6a6
Andrew Brudnak <brudnak@icloud.com>
2019-06-16 03:16:14 -0700

    Failed buffer

---
+++ 076-buffer-fail/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// THIS CODE FAILS TO RUN
+	// Only one buffer declared
+	c := make(chan int, 1)
+
+	// Not enough room with only one buffer
+	// DEADLOCK!
+	c <- 19
+	c <- 42
+
+	fmt.Println(<-c)
+}
