a4eedd461c0cb9bfb58179c7f4d3d5689403a107
Andrew Brudnak <brudnak@icloud.com>
2019-06-16 03:08:03 -0700

    Launching a Go routine for a channel

---
+++ 074-channels-intro-working/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// GO ROUTINE 1
+func main()  {
+	c := make(chan int)
+
+	// GO ROUTINE 2
+	go func() {
+		c <- 19
+	}()
+
+	// TRANSFER DATA AT SAME TIME
+	fmt.Println(<-c)
+}
+
