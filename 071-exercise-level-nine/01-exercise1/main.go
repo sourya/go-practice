e3a5ebfc9c8d1aeaf16e464244b2e9e55bf00b4e
Andrew Brudnak <brudnak@icloud.com>
2019-06-15 23:45:12 -0700

    Adds Go routine example

--- 071-exercise-level-nine/01-exercise1/main.go
+++ 071-exercise-level-nine/01-exercise1/main.go
@@ -8,23 +8,28 @@ import (
 
 func main() {
 
-	fmt.Println(runtime.NumCPU())
+	fmt.Println("begin cpu", runtime.NumCPU())
+	fmt.Println("begin gs", runtime.NumGoroutine())
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
-		fmt.Println("hello from 1st routine")
+		fmt.Println("hello from thing one")
 		wg.Done()
 	}()
 
 	go func() {
-		fmt.Println("hello from 2nd routine")
+		fmt.Println("hello from thing two")
 		wg.Done()
-
 	}()
+
+	fmt.Println("mid cpu", runtime.NumCPU())
+	fmt.Println("mid gs", runtime.NumGoroutine())
 
 	wg.Wait()
 
 	fmt.Println("about to exit")
+	fmt.Println("end cpu", runtime.NumCPU())
+	fmt.Println("end gs", runtime.NumGoroutine())
 }
