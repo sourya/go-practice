6354b73adda59e8dd85d659a0adb1666bbf9398e
Andrew Brudnak <brudnak@icloud.com>
2019-06-16 03:11:33 -0700

    Using a buffer for a Go channel

---
+++ 075-another-option/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// BUFFER OF ONE ADDED
+	// This code runs because there
+	// is a buffer of one declared for it
+	c := make(chan int, 1)
+
+		c <- 19
+
+	fmt.Println(<-c)
+}
