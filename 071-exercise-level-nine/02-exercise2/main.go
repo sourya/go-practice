76bfa4c0ed54e7667060a728fcb765894e6a4c34
Andrew Brudnak <brudnak@icloud.com>
2019-06-16 00:46:31 -0700

    Exercise complete

---
+++ 071-exercise-level-nine/02-exercise2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+}
+
+type human interface {
+	speak()
+}
+
+func (p *person) speak() {
+	fmt.Println("Hello")
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func main() {
+	p1 := person{
+		first: "Russell",
+	}
+	saySomething(&p1)
+
+	// does not work
+	// saySomething(p1)
+
+	p1.speak()
+}
