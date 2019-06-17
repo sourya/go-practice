24080ada8be49e9002256f50af82840efc8e0e84
Andrew Brudnak <brudnak@icloud.com>
2019-06-16 23:36:44 -0700

    Send only channel, does not work

--- 077-directional-channels/main.go
+++ 077-directional-channels/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int, 2)
+	// SEND ONLY CHANNEL
+	// DOES NOT WORK
+	c := make(chan <- int, 2)
 
 	c <- 19
 	c <- 42
 
+	// ERROR
 	fmt.Println(<-c)
+	// ERROR
 	fmt.Println(<-c)
 	fmt.Println("-----")
 	fmt.Printf("%T\n", c)
