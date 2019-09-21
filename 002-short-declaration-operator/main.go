965b46ea6aa45287b9d44cf4379fd82a11c51864
Andrew Brudnak <brudnak@icloud.com>
2019-09-21 01:54:14 -0700

    :clown_face: Using the short declaration operator

---
+++ 002-short-declaration-operator/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare and assign
+	x := 29
+	fmt.Println(x)
+	// assign a new value
+	x = 5
+	fmt.Println(x)
+	// variables must be used
+	y := 250 + 250
+	// no unused variables
+	fmt.Println(y)
+}
