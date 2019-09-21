8933e464c6a1102270319b4462147e1085a9516b
Andrew Brudnak <brudnak@icloud.com>
2019-09-21 16:11:28 -0700

    :muscle: First exercise complete

---
+++ 009-exercise-level-one/01-exercise/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 29
+	y := "Andrew Brudnak"
+	z := true
+
+	fmt.Printf("%v is %v years old - Has Children: %v\n", y, x, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
