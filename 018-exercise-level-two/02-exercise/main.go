b2c8378a71a629c49fc37f61c260d592681e4dbc
Andrew Brudnak <brudnak@icloud.com>
2019-09-22 23:16:18 -0700

    :muscle: 2nd exercise complete

---
+++ 018-exercise-level-two/02-exercise/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// using all of these operators
+
+	a := 19 == 19
+	b := 19 <= 20
+	c := 19 >= 18
+	d := 19 != 75
+	e := 19 < 20
+	f := 19 > 18
+
+	// all of these are always true
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
