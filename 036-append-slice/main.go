edcf090879108ce7a7719a6eabb3a5bb22529f3c
Andrew Brudnak <brudnak@icloud.com>
2019-09-24 00:53:06 -0700

    :watermelon: Appending to a slice

---
+++ 036-append-slice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{1, 2, 3, 4}
+	fmt.Println(x)
+	x = append(x, 5, 6, 7, 8)
+	fmt.Println(x)
+
+	y := []int{9, 10, 11, 12}
+	x = append(x, y...)
+	fmt.Println(x)
+}
