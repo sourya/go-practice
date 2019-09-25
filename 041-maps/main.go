6d74be63d179309228f5a0225c16374c997b93d5
Andrew Brudnak <brudnak@icloud.com>
2019-09-25 15:57:52 -0700

    :world_map: Intro to map

---
+++ 041-maps/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	m := map[string]int{
+		"Andrew": 29,
+		"Jessica": 30,
+		"Russell": 2,
+		"Ruby": 1,
+	}
+	fmt.Println(m)
+	fmt.Println(m["Russell"])
+	fmt.Println(m["Ada"])
+
+	v, ok := m["Ada"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	if v, ok := m["Ada"]; ok {
+		fmt.Println("THIS IS THE IF PRINT", v)
+	}
+
+	if v, ok := m["Andrew"]; ok {
+		fmt.Println("this should print an age:", v)
+	}
+}
