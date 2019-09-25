32c8b63c54f8c2bd37523553eb101cd6a01b0503
Andrew Brudnak <brudnak@icloud.com>
2019-09-25 16:36:27 -0700

    :muscle: First exercise complete

---
+++ 044-exercise-level-four/01-exercise/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := [5]int{1,2,3,4,5}
+	fmt.Println(x)
+
+	fmt.Printf("Type of the variable 'x': %T\n", x)
+	for i, v := range x {
+
+		fmt.Printf("Index: %v\t\t Value: %v\n", i, v)
+	}
+}
