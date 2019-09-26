5095efc5dab8571008b2dc26c4fe8d7950885edc
Andrew Brudnak <brudnak@icloud.com>
2019-09-26 10:43:16 -0700

    :world_map: Range over a map
    Russell Brudnak"s favorite ice cream flavors:
    		#1: Chocolate
    		#2: Birthday Cake
    		#3: Cherry Garcia
    Andrew Brudnak"s favorite ice cream flavors:
    		#1: Chocolate
    		#2: Green Tea
    		#3: Vanilla

--- 048-exercise-level-five/02-exercise/main.go
+++ 048-exercise-level-five/02-exercise/main.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first      string
+	last       string
+	favFlavors []string
+}
+
 func main() {
-	
+	p1 := person{
+		first: "Andrew",
+		last:  "Brudnak",
+		favFlavors: []string{
+			"Chocolate", "Green Tea", "Vanilla",
+		},
+	}
+
+	p2 := person{
+		first: "Russell",
+		last:  "Brudnak",
+		favFlavors: []string{
+			"Chocolate", "Birthday Cake", "Cherry Garcia",
+		},
+	}
+
+	m := map[string]person{
+		p1.first: p1,
+		p2.first: p2,
+	}
+
+	for _, v := range m {
+		fmt.Printf("%v %v\"s favorite ice cream flavors:\n", v.first, v.last)
+		for i, val := range v.favFlavors {
+			fmt.Printf("\t\t#%v: %v\n", i+1, val)
+		}
+	}
+
 }
