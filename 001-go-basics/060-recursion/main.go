a91753b835697e6120e545ad1a4b17925e196350
Andrew Brudnak <brudnak@icloud.com>
2019-09-28 16:33:59 -0700

    :octocat: Go is great

---
+++ 001-go-basics/060-recursion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+// recursion - a func that calls itself
+func main() {
+	n := factorial(4)
+	n1 := loopFact(4)
+
+	fmt.Println(n)
+	fmt.Println(n1)
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func loopFact(n int) int {
+	total := 1
+	for ; n > 0; n-- {
+		total *= n
+	}
+	return total
+}
