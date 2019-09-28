0812de227fc6168734f5b26273665612fb50c4c8
Andrew Brudnak <brudnak@icloud.com>
2019-09-28 15:48:41 -0700

    :octocat: Callback with Go

--- 001-go-basics/058-callback/main.go
+++ 001-go-basics/058-callback/main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	fmt.Println(sum(5, 5, 5, 5, 5))
 	fmt.Println(sum(10, 10, 10, 10, 10))
 	fmt.Println(sum(ii...))
+	fmt.Println(odd(sum, 3, 3), "ODD THINGS")
+	fmt.Println(odd(sum, ii...))
+
+	s2 := even(sum, ii...)
+	fmt.Println(s2)
+
+	fmt.Println()
+
+	s := sum(ii...)
+	fmt.Println("all numbers", s)
+
+	t1 := even(sum, ii...)
+	fmt.Println("even numbers", t1)
+
+	u1 := odd(sum, ii...)
+	fmt.Println("odd numbers", u1)
 }
 
 func sum(xi ...int) int {
@@ -15,4 +31,24 @@ func sum(xi ...int) int {
 		total += v
 	}
 	return total
+}
+
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}
+
+func odd(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 != 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
 }
