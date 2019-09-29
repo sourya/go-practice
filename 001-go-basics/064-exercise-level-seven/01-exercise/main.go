51213a2a363a225a9c64cfe5ea8f9eded8c6ef65
Andrew Brudnak <brudnak@icloud.com>
2019-09-29 02:15:05 -0700

    :muscle: First exercise complete

---
+++ 001-go-basics/064-exercise-level-seven/01-exercise/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	first := "Andrew"
+	fmt.Println("The value stored in the 'first' variable is:", first)
+	fmt.Println("The address of the value stored in 'first is:", &first)
+}
