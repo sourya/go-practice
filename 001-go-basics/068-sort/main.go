1b2d4f956ece611380ef48fa0b3b35dae9ca5334
Andrew Brudnak <brudnak@icloud.com>
2019-09-29 03:42:23 -0700

    :shipit: Starting code

---
+++ 001-go-basics/068-sort/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
+	xs := []string{"Penny", "Jessie", "Russell", "Ruby", "Andy"}
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+}
