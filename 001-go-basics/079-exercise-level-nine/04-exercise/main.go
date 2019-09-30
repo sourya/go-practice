7269059ceb9cfe076508c49f49a15ddf23eaba06
Andrew Brudnak <brudnak@icloud.com>
2019-09-30 00:34:38 -0700

    :gem: Race fixed using Mutex
    $ go run -race main.go
    1
    2
    3
    4
    5
    6
    7
    8
    9
    10
    11
    12
    13
    14
    15
    16
    17
    18
    19
    20
    21
    22
    23
    24
    25
    26
    27
    28
    29
    30
    31
    32
    33
    34
    35
    36
    37
    38
    39
    40
    41
    42
    43
    44
    45
    46
    47
    48
    49
    50
    51
    52
    53
    54
    55
    56
    57
    58
    59
    60
    61
    62
    63
    64
    65
    66
    67
    68
    69
    70
    71
    72
    73
    74
    75
    76
    77
    78
    79
    80
    81
    82
    83
    84
    85
    86
    87
    88
    89
    90
    91
    92
    93
    94
    95
    96
    97
    98
    99
    100
    end value: 100

--- 001-go-basics/079-exercise-level-nine/04-exercise/main.go
+++ 001-go-basics/079-exercise-level-nine/04-exercise/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
@@ -12,14 +11,16 @@ func main() {
 	incrementer := 0
 	gs := 100
 	wg.Add(gs)
+	var m sync.Mutex
 
 	for i := 0; i < gs; i++ {
 		go func() {
+			m.Lock()
 			v := incrementer
-			runtime.Gosched()
 			v++
 			incrementer = v
 			fmt.Println(incrementer)
+			m.Unlock()
 			wg.Done()
 		}()
 	}
