a156545e50a86ac2af46f0c789f9478a34635da9
Andrew Brudnak <brudnak@icloud.com>
2019-09-30 02:43:09 -0700

    :hammer: Adding broken example

---
+++ 001-go-basics/081-directional-channels/03-does-not-work-either/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// DOES NOT RUN
+	// RECEIVE ONLY CHANNEL
+	c := make(<- chan int, 2)
+	// TRYING to SEND fails
+	c <- 42
+	c <- 43
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println("-----")
+	fmt.Printf("%T\n", c)
+}
