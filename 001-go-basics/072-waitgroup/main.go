77deaee757f72ecace9abbfb67419a45f8a86eab
Andrew Brudnak <brudnak@icloud.com>
2019-09-29 21:41:15 -0700

    :star2: Waitgroup

--- 001-go-basics/072-waitgroup/main.go
+++ 001-go-basics/072-waitgroup/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
+func init() {
+	fmt.Println("I'll run once before func main")
+}
+
+var wg sync.WaitGroup
+
 func main() {
-	foo()
+	fmt.Println("OS\t\t\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine())
+
+	wg.Add(1)
+	go foo()
+
 	bar()
+
+	fmt.Println("CPUs\t\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine())
+	wg.Wait()
 }
 
 func foo() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("foo:", i)
 	}
+	wg.Done()
 }
 
 func bar() {
