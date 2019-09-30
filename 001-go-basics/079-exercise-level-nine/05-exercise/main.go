ddf8e44fd37cca932ee106120377fd8976a5b736
Andrew Brudnak <brudnak@icloud.com>
2019-09-30 00:40:11 -0700

    :fire: Race fixed with atomic
    $ go run -race main.go
    1
    2
    3
    4
    5
    6
    7
    17
    9
    10
    11
    12
    18
    14
    15
    8
    16
    19
    22
    20
    21
    24
    23
    13
    25
    26
    27
    28
    29
    30
    31
    32
    33
    34
    35
    36
    37
    38
    39
    40
    41
    42
    43
    44
    45
    46
    47
    48
    49
    50
    51
    52
    53
    54
    55
    56
    57
    58
    59
    60
    61
    62
    63
    64
    65
    66
    67
    68
    69
    70
    71
    72
    73
    74
    75
    76
    77
    78
    79
    80
    81
    82
    83
    84
    85
    86
    87
    88
    89
    90
    91
    92
    93
    95
    94
    96
    97
    98
    99
    100
    end value: 100

--- 001-go-basics/079-exercise-level-nine/05-exercise/main.go
+++ 001-go-basics/079-exercise-level-nine/05-exercise/main.go
@@ -3,24 +3,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
-	var wg sync.WaitGroup
 
-	incrementer := 0
+	var wg sync.WaitGroup
+	var incrementer int64
+
 	gs := 100
 	wg.Add(gs)
-	var m sync.Mutex
 
 	for i := 0; i < gs; i++ {
 		go func() {
-			m.Lock()
-			v := incrementer
-			v++
-			incrementer = v
-			fmt.Println(incrementer)
-			m.Unlock()
+			atomic.AddInt64(&incrementer, 1)
+			fmt.Println(atomic.LoadInt64(&incrementer))
 			wg.Done()
 		}()
 	}
