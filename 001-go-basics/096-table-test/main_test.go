2353e09229a773be68e54ebf82d4a33c3b300b64
Andrew Brudnak <brudnak@icloud.com>
2019-10-01 21:12:14 -0700

    :shipit: Testing with examples

--- 001-go-basics/096-table-test/main_test.go
+++ 001-go-basics/096-table-test/main_test.go
@@ -5,21 +5,21 @@ import "testing"
 func TestMySum(t *testing.T) {
 
 	type test struct {
-		data []int
+		data   []int
 		answer int
 	}
 
 	tests := []test{
-		test{[]int{21,21}, 42},
-		test{[]int{3,4,5}, 12},
-		test{[]int{1,1}, 2},
+		test{[]int{21, 21}, 42},
+		test{[]int{3, 4, 5}, 12},
+		test{[]int{1, 1}, 2},
 		test{[]int{-1, 0, -1}, 0},
 	}
 
-for _, v := range tests {
-	x := mySum(v.data...)
-	if x != v.answer {
-		
-	}
+	for _, v := range tests {
+		x := mySum(v.data...)
+		if x != v.answer {
+
+		}
 	}
 }
