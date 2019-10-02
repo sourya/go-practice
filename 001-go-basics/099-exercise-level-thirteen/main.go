fe4efb11a5585b41854d8343d80be9e428044a67
Andrew Brudnak <brudnak@icloud.com>
2019-10-01 22:04:43 -0700

    :octocat: Starting code

---
+++ 001-go-basics/099-exercise-level-thirteen/main.go
@@ -0,0 +1,19 @@
+package starting_code
+
+import (
+	"fmt"
+	"github.com/brudnak/go-practice/001-go-basics/099-exercise-level-thirteen/01-exercise/dog"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(10),
+	}
+	fmt.Println(fido)
+	fmt
