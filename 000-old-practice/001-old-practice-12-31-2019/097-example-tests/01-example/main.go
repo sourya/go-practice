10734dbfdce1d257e718e7139d3a046fe5775968
Andrew Brudnak <brudnak@icloud.com>
2019-12-31 01:28:46 -0700

    :blue_heart: Back to Go

---
+++ 000-old-practice/001-old-practice-12-31-2019/097-example-tests/01-example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	acdc2 "github.com/brudnak/go-practice/000-old-practice/001-old-practice-12-31-2019/097-example-tests/01-example/acdc"
+)
+
+func main() {
+	fmt.Println(acdc2.Sum(2, 3))
+	acdc2.Sum(10, 10, 10)
+}
