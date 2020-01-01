c0027336c3010bd1b11ba59295a60d4244885c97
Andrew Brudnak <brudnak@icloud.com>
2020-01-01 16:25:38 -0700

    :dolphin: Go refresher

---
+++ 001-go-basics/001-hello-world/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Russell & Ruby!")
+	foo()
+	fmt.Println("Something else")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+// control flow:
+// (1) sequence
+// (2) look; iterative
+// (3) conditional
