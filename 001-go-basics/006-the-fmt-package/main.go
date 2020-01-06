804bbd8a0e6e559eeee9ff1e14570ccb16f20726
Andrew Brudnak <brudnak@icloud.com>
2020-01-06 00:02:20 -0700

    :blue_heart: fmt package Go

---
+++ 001-go-basics/006-the-fmt-package/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var y = 42
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\n", y)
+	y = 911
+	fmt.Printf("%#x\n", y)
+	fmt.Printf("%#x\t%b\t%x", y, y, y)
+}
