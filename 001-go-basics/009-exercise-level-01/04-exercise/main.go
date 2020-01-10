4af2a32a137931cba1bb94643dc78a030c5bb8e8
Andrew Brudnak <brudnak@icloud.com>
2020-01-10 06:50:39 -0700

    :blue_heart: Go practice

--- 001-go-basics/009-exercise-level-01/04-exercise/main.go
+++ 001-go-basics/009-exercise-level-01/04-exercise/main.go
@@ -7,6 +7,8 @@ var myInt int
 type years int
 
 var age years
+var age2 years
+
 func main() {
 	age = 30
 	fmt.Println(age)
@@ -16,4 +18,9 @@ func main() {
 	myInt = int(age)
 	fmt.Println(myInt)
 	fmt.Printf("%T\n", myInt)
+
+	age2 = 19
+	fmt.Println(age2)
+	fmt.Printf("%T\n", age2)
+
 }
