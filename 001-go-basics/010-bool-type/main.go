29d99c0c618106fb011dbc37f734f1a6d35ccc06
Andrew Brudnak <brudnak@icloud.com>
2020-01-11 17:36:51 -0700

    :whale: Bool type

---
+++ 001-go-basics/010-bool-type/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var isMarried bool
+
+func main() {
+
+	fmt.Println(isMarried)
+	isMarried = true
+	fmt.Println(isMarried)
+
+	a := 19
+	b := 42
+	fmt.Println(a == b)
+}
