e2a8c72e5ad62f5133569e00bf4e19b1fcb05bf7
Andrew Brudnak <brudnak@icloud.com>
2020-01-12 08:27:18 -0700

    :fire: Go practice

---
+++ 001-go-basics/011-numeric-types/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+var a int
+var b float64
+
+func main() {
+	x := 19
+	y := 19.2
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+
+	a = 1
+	b = 1.2
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+
+}
