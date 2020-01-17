8e4e8f3477b6ef1942f9684ee84bf32f9616dd91
Andrew Brudnak <brudnak@icloud.com>
2020-01-16 23:54:45 -0700

    :blue_heart: It'll be fine

--- 001-go-basics/014-constants/main.go
+++ 001-go-basics/014-constants/main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 const a = 19
-const b = 75
+const b = 75.2
 const c = "Russell James"
 
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
 }
