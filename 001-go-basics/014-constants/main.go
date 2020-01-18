69a4a2e67fde1ef74d247c8766f9b84fcb4167e2
Andrew Brudnak <brudnak@icloud.com>
2020-01-17 23:56:31 -0700

    :gem: Golang practice

--- 001-go-basics/014-constants/main.go
+++ 001-go-basics/014-constants/main.go
@@ -5,13 +5,16 @@ import "fmt"
 const a = 19
 const b = 75.2
 const c = "Russell James"
+const d = "Ruby Genevieve"
 
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+	fmt.Println(d)
 
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T\n", b)
 	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", d)
 }
