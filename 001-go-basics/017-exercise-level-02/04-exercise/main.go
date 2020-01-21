8bf9ba535c090dc45dd25743db903b6f7dd879ee
Andrew Brudnak <brudnak@icloud.com>
2020-01-20 21:01:18 -0700

    :books: Golang training

--- 001-go-basics/017-exercise-level-02/04-exercise/main.go
+++ 001-go-basics/017-exercise-level-02/04-exercise/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
- var x int = 1
+var x int = 1
 
 func main() {
 	fmt.Printf("%d\t%b\t%#x\n", x, x, x)
---
+++ 001-go-basics/017-exercise-level-02/05-exercise/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := `
+1
+2
+3
+4`
+	
+	fmt.Println(s)
+}
