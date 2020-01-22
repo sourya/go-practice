cd98b173f5087becbe887977d48fd4d3466b5dba
Andrew Brudnak <brudnak@icloud.com>
2020-01-21 22:53:07 -0700

    :books: Golang practice

---
+++ 001-go-basics/021-loop-break-continue/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	for {
+		if x > 100 {
+			break
+		}
+		if x % 2 != 0 {
+			x++
+			continue
+		}
+		fmt.Println(x)
+		x++
+	}
+	fmt.Println("done.")
+}
