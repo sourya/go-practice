f0d788c77c67e8dcb9597aaa207c0318f25c937c
Andrew Brudnak <brudnak@icloud.com>
2020-01-24 22:40:31 -0700

    :bowtie: Learn Go

---
+++ 001-go-basics/031-slice-for-range/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{1, 2, 3, 4, 5}
+	fmt.Println(x[0])
+	fmt.Println(x[1])
+	fmt.Println(x[2])
+	fmt.Println(x[3])
+	fmt.Println(x[4])
+	fmt.Println("*****")
+
+	for i, v := range x{
+		fmt.Println("index", i)
+		fmt.Printf("\tvalue: %v\n", v)
+	}
+}
