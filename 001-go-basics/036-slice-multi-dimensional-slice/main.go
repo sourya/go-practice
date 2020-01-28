01b7c7843894fa69d61681d1e3ec5db001081ee3
Andrew Brudnak <brudnak@icloud.com>
2020-01-27 23:46:48 -0700

    :fire: Slice of slice

---
+++ 001-go-basics/036-slice-multi-dimensional-slice/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	rb := []string{"Ruby", "Brudnak", "Songs", "Toys"}
+	rj := []string{"Russell", "James", "Blippi", "Candy"}
+
+	xp := [][]string{rb, rj}
+
+	fmt.Println(xp)
+}
